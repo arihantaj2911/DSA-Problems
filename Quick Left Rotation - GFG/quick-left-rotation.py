2340a5e190da555e9837a6ebf9be8168fb9a8213
arihantaj2911 <119301770+arihantaj2911@users.noreply.github.com>
2023-02-13 23:43:01 +0530

    Added solution - LeetHub

--- Quick Left Rotation - GFG/quick-left-rotation.py
+++ Quick Left Rotation - GFG/quick-left-rotation.py
@@ -2,20 +2,21 @@ class Solution:
     def leftRotate(self, arr, k, n):
         # Your code goes here
         k=k%n
+        
         s=0
         e=k-1
-        while s<=e:
-            arr[s],arr[e] = arr[e],arr[s]
+        while s<e:
+            arr[s],arr[e]=arr[e],arr[s]
             s+=1
             e-=1
         
         s=k
         e=n-1
-        while s<=e:
-            arr[s],arr[e] = arr[e],arr[s]
+        while s<e:
+            arr[s],arr[e]=arr[e],arr[s]
             s+=1
             e-=1
-        
+            
         arr.reverse()
         
         
