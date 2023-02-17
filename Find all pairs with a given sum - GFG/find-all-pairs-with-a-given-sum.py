53bf550a57bc9071bb853700e58560a4e5d00119
arihantaj2911 <119301770+arihantaj2911@users.noreply.github.com>
2023-02-17 19:45:48 +0530

    Added solution - LeetHub

---
+++ Find all pairs with a given sum - GFG/find-all-pairs-with-a-given-sum.py
@@ -0,0 +1,67 @@
+#User function Template for python3
+
+class Solution:
+    def allPairs(self, A, B, N, M, X):
+        # Your code goes here 
+        # target= []
+        # for i in range(N):
+        #     for j in range(M):
+        #         if A[i]+B[j]==X:
+        #             target.append((A[i],B[j]))
+                    
+        # return target
+        l = []
+        for i in A:
+            for j in B:
+                if i + j ==X:
+                    l.append((i,j))
+        
+        l.sort()    
+        return l
+        
+                    
+                    
+    
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        sz = [int(x) for x in input().strip().split()]
+        N, M, X = sz[0], sz[1], sz[2]
+        A = [int(x) for x in input().strip().split()]
+        B = [int(x) for x in input().strip().split()]
+        ob=Solution()
+        answer = ob.allPairs(A, B, N, M, X)
+        sz = len(answer)
+        
+        if sz==0 :
+        	print(-1) 
+        
+        else: 
+            
+        	for i in range(sz):
+        		if i==sz-1:
+        		    print(*answer[i])
+        		else:
+        		    print(*answer[i], end=', ')
+        
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+# } Driver Code Ends
