4aee19fd7f5ed40eea732ee0cf6964944196c6e6
arihantaj2911 <119301770+arihantaj2911@users.noreply.github.com>
2023-03-26 17:06:38 +0530

    Added solution - LeetHub

--- Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.py
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.py
@@ -3,20 +3,39 @@
 class Solution:
     def sort012(self,arr,n):
         # code here
+        # low = 0
+        # mid = 0
+        # high = len(arr)-1
+        
+        # while mid<=high:
+        #     if arr[mid] == 0:
+        #         arr[low],arr[mid]=arr[mid],arr[low]
+        #         low +=1
+        #         mid +=1
+        #     elif arr[mid] == 1:
+        #         mid +=1
+        #     else:
+        #         arr[mid],arr[high]=arr[high],arr[mid]
+        #         high -= 1
+                
+        # return arr
+        
         low = 0
         mid = 0
         high = len(arr)-1
         
         while mid<=high:
-            if arr[mid] == 0:
-                arr[low],arr[mid]=arr[mid],arr[low]
+            if arr[mid]==0:
+                arr[low],arr[mid]= arr[mid],arr[low]
                 low +=1
                 mid +=1
-            elif arr[mid] == 1:
+            
+            elif arr[mid]==1:
                 mid +=1
+            
             else:
                 arr[mid],arr[high]=arr[high],arr[mid]
-                high -= 1
+                high -=1
                 
         return arr
 
