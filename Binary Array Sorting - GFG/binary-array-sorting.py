95b378e63c0c68b805179ed6541ccf55d7b3de0f
arihantaj2911 <119301770+arihantaj2911@users.noreply.github.com>
2023-07-30 20:36:31 +0530

    Added solution - LeetHub

---
+++ Binary Array Sorting - GFG/binary-array-sorting.py
@@ -0,0 +1,38 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to sort the binary array.
+    def binSort(self, A, N): 
+        #Your code here
+        #No need to print the array
+        arr = A.sort()
+        return arr
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            N=int(input())
+            A=list(map(int,input().split()))
+            obj = Solution()
+            obj.binSort(A,N)
+            
+            for i in A:
+                print(i,end=" ")
+            print()
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
