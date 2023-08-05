46f4b56a66f8bd1ddfc470fc0222a097484af2bb
arihantaj2911 <119301770+arihantaj2911@users.noreply.github.com>
2023-08-05 15:58:46 +0530

    Added solution - LeetHub

--- Check if array is sorted - GFG/check-if-array-is-sorted.py
+++ Check if array is sorted - GFG/check-if-array-is-sorted.py
@@ -3,11 +3,11 @@
 class Solution:
     def arraySortedOrNot(self, arr, n):
         # code here
-        for i in range(1,n):
-            if arr[i-1]>arr[i]:
-                return 0
-                
-        return 1
+        for i in range(0,n-1):
+            if arr[i]>arr[i+1]:
+                return False
+        return True
+
 
 #{ 
  # Driver Code Starts
